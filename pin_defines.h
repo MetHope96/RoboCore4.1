52e6e17091025c3296d181140e0bb832bcd98eb7
Mattia Dipaola <106329785+MetHope96@users.noreply.github.com>
2025-10-28 21:16:00 +0100

    Add pin definitions for various peripherals

---
+++ pin_defines.h
@@ -0,0 +1,46 @@
+#ifndef PIN_DEFINES_H
+#define PIN_DEFINES_H
+
+#define RX_1        0
+#define TX_1        1
+#define LED1        2
+#define LED2        3
+#define LED3        4
+#define LED4        5
+#define LED5        6
+#define RX_2        7
+#define TX_2        8
+#define BUZZ        9
+#define CS          10
+#define MOSI        11
+#define MISO        12
+#define SCK         13
+#define RX_3        15
+#define TX_3        14
+#define SDA         18
+#define SCL         19
+#define TX_5        20
+#define RX_5        21
+#define CAN_TX      22
+#define CAN_RX      23
+#define A1          24
+#define A2          25
+#define MOSI1       26
+#define SCK1        27
+#define RX_7        28
+#define TX_7        29
+#define CAN1_RX     30
+#define CAN1_TX     31
+#define SCL1        16
+#define SDA1        17
+#define D1          33
+#define D2          34
+#define D3          35
+#define D4          36
+#define D5          37
+#define CS1         38
+#define MISO1       39
+#define A3          40
+#define A4          41
+
+#endif // PIN_DEFINES_H
